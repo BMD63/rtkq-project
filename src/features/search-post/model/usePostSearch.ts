c810205b28b767eebe85167b1473f4c4fbb3b903
Sergei <chechet@mail.ru>
2026-04-10 19:25:23 +0400

    feat: add debounce hook, add debounce on search

--- src/features/search-post/model/usePostSearch.ts
+++ src/features/search-post/model/usePostSearch.ts
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import type { Post } from '@/entities/post/model/types'
+import { useDebounce } from '@/shared/lib/hooks/useDebounce'
+
 
 export const usePostSearch = (posts: Post[] | undefined) => {
   const [query, setQuery] = useState('')
 
+  const debouncedQuery = useDebounce(query, 300)
+
   const filteredPosts = (posts ?? []).filter((post) =>
-    post.title.toLowerCase().includes(query.toLowerCase())
+    post.title.toLowerCase().includes(debouncedQuery.toLowerCase())
   )
 
   return {
