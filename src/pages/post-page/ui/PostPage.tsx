f164610e6c7b64ddcd4c2bcb5315f29df66ad306
Sergei <chechet@mail.ru>
2026-04-10 15:07:17 +0400

    feat: add post adding

--- src/pages/post-page/ui/PostPage.tsx
+++ src/pages/post-page/ui/PostPage.tsx
@@ -1,8 +1,10 @@
-import { useGetPostsQuery, useDeletePostMutation } from '@/shared/api/baseApi'
+import { useGetPostsQuery, useDeletePostMutation, useCreatePostMutation } from '@/shared/api/baseApi'
 
 export const PostsPage = () => {
   const { data, isLoading, isError } = useGetPostsQuery()
   const [deletePost] = useDeletePostMutation()
+  const [createPost] = useCreatePostMutation()
+
 
   if (isLoading) return <div>Loading...</div>
   if (isError) return <div>Error</div>
@@ -11,10 +13,18 @@ export const PostsPage = () => {
     await deletePost(id)
   }
 
+  const handleAdd = async () => {
+  await createPost({
+    title: 'New post',
+    body: 'Some text',
+    userId: 1,
+  })
+}
+
   return (
     <div>
       <h1>Posts</h1>
-
+      <button onClick={handleAdd}>Add Post</button>
       {data?.map((post) => (
         <div key={post.id}>
           <h3>{post.title}</h3>
--- src/shared/api/baseApi.ts
+++ src/shared/api/baseApi.ts
@@ -13,31 +13,58 @@ export const baseApi = createApi({
       providesTags: ['Post'],
     }),
     deletePost: build.mutation<void, number>({
-    query: (id) => ({
-      url: `/posts/${id}`,
-      method: 'DELETE',
-    }),
-    async onQueryStarted(id, { dispatch, queryFulfilled }) {
-    // 1. оптимистично обновляем кеш
-      const patchResult = dispatch(
-        baseApi.util.updateQueryData('getPosts', undefined, (draft) => {
-          return draft.filter((post) => post.id !== id)
-        })
-      )
+      query: (id) => ({
+        url: `/posts/${id}`,
+        method: 'DELETE',
+      }),
+      async onQueryStarted(id, { dispatch, queryFulfilled }) {
+      // 1. оптимистично обновляем кеш
+        const patchResult = dispatch(
+          baseApi.util.updateQueryData('getPosts', undefined, (draft) => {
+            return draft.filter((post) => post.id !== id)
+          })
+        )
 
-      try {
-        // 2. ждём реальный запрос
-        await queryFulfilled
-      } catch {
-        // 3. если ошибка — откатываем
-        patchResult.undo()
-      }
-    },
-  }),
+        try {
+          // 2. ждём реальный запрос
+          await queryFulfilled
+        } catch {
+          // 3. если ошибка — откатываем
+          patchResult.undo()
+        }
+      },
+    }),
+    createPost: build.mutation<Post, Partial<Post>>({
+      query: (body) => ({
+        url: '/posts',
+        method: 'POST',
+        body,
+      }),
+
+      async onQueryStarted(newPost, { dispatch, queryFulfilled }) {
+        const patchResult = dispatch(
+          baseApi.util.updateQueryData('getPosts', undefined, (draft) => {
+            draft.unshift({
+              id: Date.now(), // временный id
+              userId: newPost.userId ?? 1,
+              title: newPost.title ?? '',
+              body: newPost.body ?? '',
+            })
+          })
+        )
+
+        try {
+          await queryFulfilled
+        } catch {
+          patchResult.undo()
+        }
+      },
+    })
   }),
 })
 
 export const {
   useGetPostsQuery,
   useDeletePostMutation,
+  useCreatePostMutation,
 } = baseApi
